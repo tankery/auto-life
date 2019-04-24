fc33f0f82b38095af3616f3d75be661ad7f90806
Tankery Chen <xjchen@mobvoi.com>
2019-04-24 19:27:06 +0800

    Fix issue that command can not execute out of project dir

--- autolife.py
+++ autolife.py
@@ -5,8 +5,9 @@ __author__ = "Tankery Chen"
 # import all packages
 from os.path import dirname, isdir, isfile, join
 from os import listdir
-dirs = listdir(dirname(__file__))
-modules = [ __import__(f) for f in dirs if isdir(f) and isfile(join(f, '__init__.py'))]
+workdir = dirname(__file__)
+dirs = listdir(workdir)
+modules = [ __import__(f) for f in dirs if isdir(join(workdir, f)) and isfile(join(workdir, f, '__init__.py'))]
 
 main = click.CommandCollection(
     sources=[getattr(g, 'group') for g in modules if hasattr(g, 'group')],
